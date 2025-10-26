2bae0d503a718691edf1c8784aca8a7669b9a92f
Roman Josipović <rjosipovic@gmail.com>
2025-10-26 22:44:58 +0100

    Introducing actuator in gamification-manager

--- apps/math-challenges/gamification-manager/src/main/java/com/playground/gamification_manager/config/SecurityConfig.java
+++ apps/math-challenges/gamification-manager/src/main/java/com/playground/gamification_manager/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -20,8 +21,20 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final AuthConfig authConfig;
+    private final ManagementConfig managementConfig;
 
     @Bean
+    @Order(1)
+    public SecurityFilterChain managementSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .securityMatcher(new PortRequestMatcher(managementConfig.getServer().getPort()))
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+
+    @Bean
+    @Order(2)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
