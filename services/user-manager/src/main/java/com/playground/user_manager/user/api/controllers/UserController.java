f01520daaa5d31a411091951aadae78beda54d58
Roman Josipović <rjosipovic@gmail.com>
2026-05-15 13:42:06 +0200

    Worklow trigger commit

--- services/user-manager/src/main/java/com/playground/user_manager/user/api/controllers/UserController.java
+++ services/user-manager/src/main/java/com/playground/user_manager/user/api/controllers/UserController.java
@@ -14,6 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * User Controller
+ */
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
